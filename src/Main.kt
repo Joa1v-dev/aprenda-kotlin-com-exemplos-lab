eeef85ace2fce14ffacf4d3c6414422f23c06694
Joa1v-dev <devjoa1v@gmail.com>
2025-10-02 21:16:57 -0300

    organização do código e implementação de novas funcionalidades.

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,33 @@
+import java.text.Normalizer
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 data class Usuario(val nome: String, val email: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val professor: String)
-
 data class Professor(val nome: String)
 
-data class Formacao(val nome: String, val nivel: Nivel,var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(var nome: String,
+                               val duracao: Int = 60,
+                               val professor: Professor
+)
 
+data class Formacao(val nome: String,
+                    val nivel: Nivel,
+                    var conteudos: List<ConteudoEducacional>
+) {
     val inscritos = mutableListOf<Usuario>()
 
     val tempoCurso: Int
         get() = conteudos.sumOf { it.duracao }
+
+    val tempoFormatado: String
+        get() {
+            val horas = tempoCurso / 60
+            val minutos = tempoCurso % 60
+            return "${horas}h ${minutos}min"
+        }
 
     fun matricular(usuario: Usuario) {
         //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
@@ -22,34 +36,95 @@ data class Formacao(val nome: String, val nivel: Nivel,var conteudos: List<Conte
     }
 
     fun detalhes() {
-        println("Curso: $nome")
-        println("Nível: $nivel")
-        println("Conteúdos: ")
-        println("Duração total do curso: $tempoCurso")
+        println("\n==================================")
+        println("  Curso: $nome")
+        println("  Nível: $nivel")
+        println("  Duração total do curso: $tempoFormatado")
+        println("  Conteúdos:")
         conteudos.forEachIndexed { index, conteudo ->
-            println(" ${index + 1}.${conteudo.nome}")
+            println("    ${index + 1}.${conteudo.nome}")
             println("    Duração: ${conteudo.duracao} min")
-            println("    Professor: ${conteudo.professor}\n")
+            println("    Professor: ${conteudo.professor.nome}\n")
+
         }
+        if (inscritos.isNotEmpty()) {
+            println("Inscritos: ")
+            inscritos.forEach { println(" - ${it.nome} (${it.email})") }
+        }
+        println("--------------------------------\n")
     }
 }
+
+// funções auxiliares
+
+fun criarUsuarios() = listOf(
+    Usuario("João Victor Alexandre", "joao@email.com",22),
+    Usuario("Layla Carvalho", "laylac@email.com",26),
+    Usuario("Lucas Santos", "lucass@email.com", 30),
+    Usuario("Paulo Silva", "paulos@email.com", 23),
+    Usuario("Ana Costa", "anac@email.com",29)
+)
+
+fun criarProfessores() = listOf(
+    Professor("Ben Matos"),
+    Professor("Flavio Abreu"),
+    Professor("Raimundo Sousa"),
+    Professor("Camila Andrade"),
+    Professor("Silvana Lima")
+)
+
+fun criarCursos(professor: List<Professor>): List<Formacao> {
+    val conteudosKotlin = listOf(
+        ConteudoEducacional("Introdução à Programação Kotlin",60, professor[0]),
+        ConteudoEducacional("Avançando na Programação Kotlin", 120, professor[1]),
+        ConteudoEducacional("Desafio de Projeto Kotlin", 90, professor[0]),
+        )
+
+    val conteudosJava = listOf(
+        ConteudoEducacional("Introdução a Linguagem Java", 120, professor[3]),
+        ConteudoEducacional("Avançando na Linguagem Java", 180, professor[3]),
+    )
+
+    val conteudosWeb = listOf(
+        ConteudoEducacional("Programação Web com HTML e CSS", 140, professor[4]),
+        ConteudoEducacional("Programação Web Aprendendo JavaScript", 120, professor[4])
+    )
+
+
+    return listOf(
+        Formacao("Kotlin Básico ao Avançado", Nivel.INTERMEDIARIO, conteudosKotlin),
+        Formacao("Java Básico ao Avançado", Nivel.DIFICIL, conteudosJava),
+        Formacao("Programação Web", Nivel.BASICO, conteudosWeb)
+
+    )
+}
+
+
+
+fun criarCurso(nome: String, nivel: Nivel, conteudos: List<ConteudoEducacional>) : Formacao {
+    return Formacao(nome, nivel, conteudos)
+}
+
 
 fun main() {
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 
-    val usuario1 = Usuario("João Victor", "joao@email.com", 22)
-    val usuario2 = Usuario("Layla", "layla@email.com", 26)
+    val professores = criarProfessores()
+    val usuarios = criarUsuarios()
+    val cursos = criarCursos(professores)
 
-    val professor1 = Professor("Ben Matos")
-    val professor2 = Professor("Flavio Abreu")
+    val cursoKotlin = cursos[0]
+    val cursoJava = cursos[1]
+    val cursoWeb = cursos[2]
 
-    val conteudo1 = ConteudoEducacional("Introdução a programação Kotlin", 60, professor1.nome)
-    val conteudo2 = ConteudoEducacional("Avançando na Programação Kotlin", 120, professor2.nome)
+    cursoKotlin.matricular(usuarios[0])
+    cursoKotlin.matricular(usuarios[1])
 
-    val cursoKotlin = Formacao("Curso de Kotlin Básico ao Avançado", Nivel.BASICO, listOf(conteudo1,conteudo2),)
-    cursoKotlin.detalhes()
-    cursoKotlin.matricular(usuario1)
-    cursoKotlin.matricular(usuario2)
+    cursoWeb.matricular(usuarios[2])
+    cursoWeb.matricular(usuarios[3])
 
+    cursoJava.matricular(usuarios[4])
+
+    cursos.forEach { it.detalhes() }
 }
